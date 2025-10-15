eac081347c8c7286dffcede039bac92f9d4b388e
a114221045-Lai <a114221045@mail.shu.edu.tw>
2025-10-15 12:53:39 +0800

    114-10-15

---
+++ 114_10_15/From1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace GroupBoxDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.groupBox1.Location = new System.Drawing.Point(52, 32);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(319, 183);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "GBDemo";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Font = new System.Drawing.Font("新細明體", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.panel1.Location = new System.Drawing.Point(404, 94);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(293, 287);
+            this.panel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(192, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(80, 118);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(169, 57);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ 114_10_15/Tutorial 4-3/Loan Qualifier/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Loan_Qualifier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要的設計工具變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除正在使用的資源。
+        /// </summary>
+        /// <param name="disposing">如果應釋放受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計產生的程式碼
+
+        /// <summary>
+        /// 設計工具支援所需的方法 - 請勿以程式碼編輯器修改
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.salaryPromptLabel = new System.Windows.Forms.Label();
+            this.yearsPromptLabel = new System.Windows.Forms.Label();
+            this.salaryTextBox = new System.Windows.Forms.TextBox();
+            this.yearsTextBox = new System.Windows.Forms.TextBox();
+            this.outputDescriptionLabel = new System.Windows.Forms.Label();
+            this.decisionLabel = new System.Windows.Forms.Label();
+            this.checkButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // salaryPromptLabel
+            // 
+            this.salaryPromptLabel.AutoSize = true;
+            this.salaryPromptLabel.Font = new System.Drawing.Font("微軟正黑體", 18F);
+            this.salaryPromptLabel.Location = new System.Drawing.Point(223, 51);
+            this.salaryPromptLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.salaryPromptLabel.Name = "salaryPromptLabel";
+            this.salaryPromptLabel.Size = new System.Drawing.Size(128, 45);
+            this.salaryPromptLabel.TabIndex = 0;
+            this.salaryPromptLabel.Text = "年薪：";
+            // 
+            // yearsPromptLabel
+            // 
+            this.yearsPromptLabel.AutoSize = true;
+            this.yearsPromptLabel.Font = new System.Drawing.Font("微軟正黑體", 18F);
+            this.yearsPromptLabel.Location = new System.Drawing.Point(151, 157);
+            this.yearsPromptLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.yearsPromptLabel.Name = "yearsPromptLabel";
+            this.yearsPromptLabel.Size = new System.Drawing.Size(200, 45);
+            this.yearsPromptLabel.TabIndex = 1;
+            this.yearsPromptLabel.Text = "現職年資：";
+            // 
+            // salaryTextBox
+            // 
+            this.salaryTextBox.Font = new System.Drawing.Font("微軟正黑體", 18F);
+            this.salaryTextBox.Location = new System.Drawing.Point(446, 41);
+            this.salaryTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.salaryTextBox.Name = "salaryTextBox";
+            this.salaryTextBox.Size = new System.Drawing.Size(332, 55);
+            this.salaryTextBox.TabIndex = 2;
+            // 
+            // yearsTextBox
+            // 
+            this.yearsTextBox.Font = new System.Drawing.Font("微軟正黑體", 18F);
+            this.yearsTextBox.Location = new System.Drawing.Point(446, 147);
+            this.yearsTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.yearsTextBox.Name = "yearsTextBox";
+            this.yearsTextBox.Size = new System.Drawing.Size(148, 55);
+            this.yearsTextBox.TabIndex = 3;
+            // 
+            // outputDescriptionLabel
+            // 
+            this.outputDescriptionLabel.AutoSize = true;
+            this.outputDescriptionLabel.Font = new System.Drawing.Font("微軟正黑體", 18F);
+            this.outputDescriptionLabel.Location = new System.Drawing.Point(79, 268);
+            this.outputDescriptionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.outputDescriptionLabel.Name = "outputDescriptionLabel";
+            this.outputDescriptionLabel.Size = new System.Drawing.Size(272, 45);
+            this.outputDescriptionLabel.TabIndex = 4;
+            this.outputDescriptionLabel.Text = "貸款審核結果：";
+            // 
+            // decisionLabel
+            // 
+            this.decisionLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.decisionLabel.Font = new System.Drawing.Font("微軟正黑體", 18F);
+            this.decisionLabel.Location = new System.Drawing.Point(446, 256);
+            this.decisionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.decisionLabel.Name = "decisionLabel";
+            this.decisionLabel.Size = new System.Drawing.Size(542, 64);
+            this.decisionLabel.TabIndex = 5;
+            this.decisionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // checkButton
+            // 
+            this.checkButton.Font = new System.Drawing.Font("微軟正黑體", 18F);
+            this.checkButton.Location = new System.Drawing.Point(151, 383);
+            this.checkButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(213, 65);
+            this.checkButton.TabIndex = 6;
+            this.checkButton.Text = "檢查資格";
+            this.checkButton.UseVisualStyleBackColor = true;
+            this.checkButton.Click += new System.EventHandler(this.checkButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("微軟正黑體", 18F);
+            this.clearButton.Location = new System.Drawing.Point(491, 383);
+            this.clearButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(132, 65);
+            this.clearButton.TabIndex = 7;
+            this.clearButton.Text = "清除";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("微軟正黑體", 18F);
+            this.exitButton.Location = new System.Drawing.Point(824, 383);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(128, 65);
+            this.exitButton.TabIndex = 8;
+            this.exitButton.Text = "離開";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1126, 500);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.checkButton);
+            this.Controls.Add(this.decisionLabel);
+            this.Controls.Add(this.outputDescriptionLabel);
+            this.Controls.Add(this.yearsTextBox);
+            this.Controls.Add(this.salaryTextBox);
+            this.Controls.Add(this.yearsPromptLabel);
+            this.Controls.Add(this.salaryPromptLabel);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "貸款資格審核";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        // 表單元件宣告區
+        private System.Windows.Forms.Label salaryPromptLabel;      // 年薪標籤
+        private System.Windows.Forms.Label yearsPromptLabel;       // 現職年資標籤
+        private System.Windows.Forms.TextBox salaryTextBox;        // 年薪輸入框
+        private System.Windows.Forms.TextBox yearsTextBox;         // 年資輸入框
+        private System.Windows.Forms.Label outputDescriptionLabel;  // 貸款審核結果說明標籤
+        private System.Windows.Forms.Label decisionLabel;           // 審核結果顯示標籤
+        private System.Windows.Forms.Button checkButton;            // 檢查資格按鈕
+        private System.Windows.Forms.Button clearButton;            // 清除按鈕
+        private System.Windows.Forms.Button exitButton;             // 離開按鈕
+    }
+}
+
